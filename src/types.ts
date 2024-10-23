d3e616489c9317be7c4f6906496d23986fe860c1
AmericanJesus-Tech <masonpfingston@gmail.com>
2024-10-23 08:42:12 -0700

    Add files via upload

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,15 @@
- export type Slide = { 
-    id: number, 
-    order: number, 
-    image: string, 
-    text: string, 
-    fontColor: string,
-    speakerNotes: string,
-    fontFamily: string,
-    fontSize: string,
+export type CartItem = {
+    id: number,
+    productId: number,
+    amount: number,
 }
+export type Product ={
+    "id": number,
+    "name": string,
+    "brand": string,
+    "price": number,
+    "flavor": string,
+    "weight": string,
+    "ingredients": string[],
+    "inStock": boolean
+  }
